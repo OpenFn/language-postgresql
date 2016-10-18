9919c366cbe87724acb957531ae23bf5c4b0bf28
taylordowns2000 <downs.taylor@gmail.com>
2016-10-18 16:00:09 +0200

    basic operation with insert

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -67,62 +67,54 @@ export function insert(table, rowData) {
       sql.query
     )
 
-    // return new Promise((resolve, reject) => {
+    console.log(body)
 
-      var pg = require('pg');
+    // create a config to configure both pooling behavior
+    // and client options
+    // note: all config is optional and the environment variables
+    // will be read if the config is not present
+    var config = {
+      host: host,
+      port: port,
+      database: database,
+      user: user,
+      password: password,
+      ssl: true
+    };
 
-      // create a config to configure both pooling behavior
-      // and client options
-      // note: all config is optional and the environment variables
-      // will be read if the config is not present
-      var config = {
-        host: host,
-        port: port,
-        database: database,
-        user: user,
-        password: password,
-        ssl: true
-      };
+    //this initializes a connection pool
+    //it will keep idle connections open for a 30 seconds
+    //and set a limit of maximum 10 idle clients
+    var pool = new pg.Pool(config);
 
-      //this initializes a connection pool
-      //it will keep idle connections open for a 30 seconds
-      //and set a limit of maximum 10 idle clients
-      var pool = new pg.Pool(config);
+    // to run a query we can acquire a client from the pool,
+    // run a query on the client, and then return the client to the pool
+    pool.connect(function(err, client, done) {
+      if(err) {
+        return console.error('error fetching client from pool', err);
+      }
+      client.query(body, function(err, result) {
+        // call `done()` to release the client back to the pool
+        done();
 
-      // to run a query we can acquire a client from the pool,
-      // run a query on the client, and then return the client to the pool
-      pool.connect(function(err, client, done) {
         if(err) {
-          return console.error('error fetching client from pool', err);
+          return console.error('error running query', err);
         }
-        client.query('SELECT NOW() as time', function(err, result) {
-          // call `done()` to release the client back to the pool
-          done();
-
-          if(err) {
-            return console.error('error running query', err);
-          }
-          console.log(result);
-          //output: 1
-        });
+        console.log(result);
+        //output: 1
       });
+    });
 
-      pool.on('error', function (err, client) {
-        // if an error is encountered by a client while it sits idle in the pool
-        // the pool itself will emit an error event with both the error and
-        // the client which emitted the original error
-        // this is a rare occurrence but can happen if there is a network partition
-        // between your application and the database, the database restarts, etc.
-        // and so you might want to handle it and at least log it out
-        console.error('idle client error', err.message, err.stack)
-      })
-
-    // })
-
-    // .then((data) => {
-    //   const nextState = { ...state, response: { body: data } };
-    //   return nextState;
-    // })
+    pool.on('error', function (err, client) {
+      // if an error is encountered by a client while it sits idle in the pool
+      // the pool itself will emit an error event with both the error and
+      // the client which emitted the original error
+      // this is a rare occurrence but can happen if there is a network
+      //  partition between your application and the database, the database
+      //  restarts, etc.
+      // and so you might want to handle it and at least log it out
+      console.error('idle client error', err.message, err.stack)
+    })
 
   }
 }
