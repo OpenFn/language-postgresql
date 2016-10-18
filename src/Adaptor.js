99dc835d824a152a3e5b724396be30535b261d02
taylordowns2000 <downs.taylor@gmail.com>
2016-10-18 13:48:46 +0200

    structure

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -1,6 +1,8 @@
 import { execute as commonExecute, expandReferences } from 'language-common';
-import pg from 'pg';
 import { resolve as resolveUrl } from 'url';
+
+var pg = require('pg');
+var jsonSql = require('json-sql')();
 
 /** @module Adaptor */
 
@@ -42,30 +44,80 @@ export function post(url, {body, callback, headers}) {
 
   return state => {
 
+    const {
+      username,
+      password,
+      database,
+      host,
+      port
+    } = state.configuration;
+
+    const dataObject = expandReferences(rowData)(state);
+
+    const sql = jsonSql.build({
+        type: 'insert',
+        table: table,
+        values: dataObject
+    });
+
+    const body = Object.keys(sql.values).reduce(
+      function(query, valueKey) {
+        return query.replace(`\$${valueKey}`, `'${sql.values[valueKey]}'`)
+      },
+      sql.query
+    )
+
     return new Promise((resolve, reject) => {
 
-      // instantiate a new client
-      // the client will read connection information from
-      // the same environment variables used by postgres cli tools
-      var client = new pg.Client();
+      var pg = require('pg');
 
-      // connect to our database
-      client.connect(function (err) {
-        if (err) throw err;
+      // create a config to configure both pooling behavior
+      // and client options
+      // note: all config is optional and the environment variables
+      // will be read if the config is not present
+      var config = {
+        user: username, //env var: PGUSER
+        database: database, //env var: PGDATABASE
+        password: password, //env var: PGPASSWORD
+        port: port, //env var: PGPORT
+        max: 10, // max number of clients in the pool
+        idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+      };
 
-        // execute a query on our database
-        client.query('SELECT $1::text as name', ['brianc'], function (err, result) {
-          if (err) throw err;
 
-          // just print the result to the console
-          console.log(result.rows[0]); // outputs: { name: 'brianc' }
+      //this initializes a connection pool
+      //it will keep idle connections open for a 30 seconds
+      //and set a limit of maximum 10 idle clients
+      var pool = new pg.Pool(config);
 
-          // disconnect the client
-          client.end(function (err) {
-            if (err) throw err;
-          });
+      // to run a query we can acquire a client from the pool,
+      // run a query on the client, and then return the client to the pool
+      pool.connect(function(err, client, done) {
+        if(err) {
+          return console.error('error fetching client from pool', err);
+        }
+        // client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+        client.query(body, ['1'], function(err, result) {
+          //call `done()` to release the client back to the pool
+          done();
+
+          if(err) {
+            return console.error('error running query', err);
+          }
+          console.log(result.rows[0].number);
+          //output: 1
         });
       });
+
+      pool.on('error', function (err, client) {
+        // if an error is encountered by a client while it sits idle in the pool
+        // the pool itself will emit an error event with both the error and
+        // the client which emitted the original error
+        // this is a rare occurrence but can happen if there is a network partition
+        // between your application and the database, the database restarts, etc.
+        // and so you might want to handle it and at least log it out
+        console.error('idle client error', err.message, err.stack)
+      })
 
     }).then((data) => {
       const nextState = { ...state, response: { body: data } };
