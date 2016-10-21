9e29ff0750aca030a757b5aec67653ad1be43950
taylordowns2000 <downs.taylor@gmail.com>
2016-10-21 18:32:11 +0200

    erroring correctly

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -55,9 +55,7 @@ export function sql(sqlQuery) {
     const body = sqlQuery(state);
     console.log("Executing SQL statement: " + body)
 
-    // create a config to configure both pooling behavior and client options
-    // note: all config is optional and the environment variables will be read
-    // if the config is not present.
+    // setup client config
     var config = {
       host: host,
       port: port,
@@ -67,45 +65,23 @@ export function sql(sqlQuery) {
       ssl: true
     };
 
-    //this initializes a connection pool
-    //it will keep idle connections open for a 30 seconds
-    //and set a limit of maximum 10 idle clients
-    var pool = new pg.Pool(config);
+    // instantiate a new client
+    var client = new pg.Client(config);
 
     return new Promise((resolve, reject) => {
-      // to run a query we can acquire a client from the pool,
-      // run a query on the client, and then return the client to the pool
-      pool.connect(function(err, client, done) {
-        if(err) {
-          return console.error('error fetching client from pool', err);
-          reject(err)
-        } else {
-          client.query(body, function(err, result) {
-            // call `done()` to release the client back to the pool
-            resolve(done())
-            if(err) {
-              return console.error('error running query', err);
-              reject(err)
-            } else {
-              console.log(result);
-              resolve("OK.")
-            }
+      // connect to our database
+      client.connect(function(err) {
+        if (err) throw err;
+        // execute a query on our database
+        client.query(body, function(err, result) {
+          if (err) throw err;
+          // print the result to the console
+          console.log(result);
+          // disconnect the client
+          client.end(function (err) {
+            if (err) throw err;
           })
-        }
-      })
-    })
-
-    .then((result) => {
-      pool.on('error', function (err, client) {
-        // if an error is encountered by a client while it sits idle in the pool
-        // the pool itself will emit an error event with both the error and
-        // the client which emitted the original error
-        // this is a rare occurrence but can happen if there is a network
-        //  partition between your application and the database, the database
-        //  restarts, etc.
-        // and so you might want to handle it and at least log it out
-        console.error('idle client error', err.message, err.stack)
-        reject(err)
+        })
       })
     })
 
